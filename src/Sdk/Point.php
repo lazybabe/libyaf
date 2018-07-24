83df6ef1740e1300474c3a342f4662019633d4c9
秦宇光 <qinyuguang@betime.com>
2018-07-24 15:48:28 +0800

    point sdk

---
+++ src/Sdk/Point.php
@@ -0,0 +1,43 @@
+<?php
+namespace Libyaf\Sdk;
+
+class Point extends Base\Client
+{
+    public function getPoint($uid)
+    {
+        $params = [
+            'uid' => $uid,
+        ];
+
+        $data = $this->get('user', 'point', $params);
+
+        return json_decode($data, true);
+    }
+
+    public function getLevel($uid)
+    {
+        $params = [
+            'uid' => $uid,
+        ];
+
+        $data = $this->get('user', 'level', $params);
+
+        return json_decode($data, true);
+    }
+
+    public function consume($uid, $point, $context)
+    {
+        $params = [
+            'uid'       => $uid,
+            'point'     => $point,
+            'context'   => is_array($context) ? json_encode($context) : $context,
+        ];
+
+        $data = $this->post('user', 'consume', $params);
+
+        return json_decode($data, true);
+    }
+
+}
+
+
