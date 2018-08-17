97f7ba501ba7bf0fb626dcaf8710d1d20604c300
秦宇光 <qinyuguang@betime.com>
2018-08-17 18:00:34 +0800

    push

--- src/Sdk/Sms.php
+++ src/Sdk/Sms.php
@@ -29,6 +29,40 @@ class Sms extends Base\Client
         return json_decode($data, true);
     }
 
+    /**
+     * @brief 发送推送
+     *
+     * @param string $content    发送内容
+     * @param string $group      配置组如betime
+     * @param string $extras     自定义设置 如 alias|extras|title
+     *
+     * @return array 执行结果
+     */
+    public function push($content, $group = 'betime', array $extras = [])
+    {
+        $params = [
+            'content'   => $content,
+            'group'     => $group,
+        ];
+
+        $params = $params + $extras;
+
+        if ($extras['alias']) {
+            if (is_string($extras['alias'])) {
+                $extras['alias'] = [$extras['alias']];
+            }
+            $params['alias'] = json_encode($extras['alias']);
+        }
+
+        if ($extras['extras'] && is_array($extras['extras'])) {
+            $params['extras'] = json_encode($extras['extras']);
+        }
+
+        $data = $this->post('push', 'send', $params);
+
+        return json_decode($data, true);
+    }
+
 }
 
 
